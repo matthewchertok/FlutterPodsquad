c26eeef8ab4a927fcbaf21b80de218766948761e
blizzardwestie <71357433+blizzardwestie@users.noreply.github.com>
2021-06-16 00:31:58 -0600

    hhhhh

--- lib/main.dart
+++ lib/main.dart
@@ -26,28 +26,8 @@ class _AppState extends State<MyApp> {
   final Future<FirebaseApp> _initialization = Firebase.initializeApp();
   FirebaseMessaging _messaging = FirebaseMessaging.instance;
 
-  ///This function can read push notification payload data and open a specified view.
-  void respondToPushNotification() async {
-    //Get any messages which caused the application to open from
-    // a terminated state.
-    RemoteMessage? initialMessage = await FirebaseMessaging.instance.getInitialMessage();
 
-    // If the message also contains a data property with a "type" of "none",
-    // navigate to the main screen
-    if (initialMessage?.data['notificationType'] == null) {
-      Navigator.pushNamed(context, '/LoadingView');
-    } else if (initialMessage?.notification?.title == "Test Notification") Navigator.pushNamed(context, '/LoadingView');
-
-    // Also handle any interaction when the app is in the background via a
-    // Stream listener
-    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
-      if (message.data['notificationType'] == 'xxx') {
-        Navigator.pushNamed(context, '/StartingView');
-      } else
-       // Navigator.push(context, CupertinoPageRoute(builder: (context) => LoadingView()));
-      print("BIDEN");
-    });
-  }
+  
 
   @override
   Widget build(BuildContext context) {
@@ -67,7 +47,7 @@ class _AppState extends State<MyApp> {
           print("FlutterFire initialized!");
           if (Platform.isIOS) _messaging.requestPermission();
           _messaging.subscribeToTopic("TEST_TOPIC");
-          respondToPushNotification();
+       //   respondToPushNotification();
           return CupertinoApp(home: StartingView());
         }
 

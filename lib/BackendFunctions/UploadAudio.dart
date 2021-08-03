72f54ee53e77f61bf9bc835759e0993cc55e52d9
blizzardwestie <71357433+blizzardwestie@users.noreply.github.com>
2021-08-02 22:18:30 -0600

    Undid the previous change
    It turns out that iOS adds "file:///" in front of the path regardless of what the path actually is. So my previous code was wrong.

--- lib/BackendFunctions/UploadAudio.dart
+++ lib/BackendFunctions/UploadAudio.dart
@@ -13,11 +13,10 @@ class UploadAudio {
   Future<List<String>?> uploadRecordingToDatabase(
       {required File recordingFile, required String chatPartnerOrPodID, required bool isPodMessage}) async {
 
-    // for strange reasons, the file path sometimes gets messed up on iOS (an extra "file:///" gets added in front of
-    // the path)
-    // Thus, to fix this, I must remove that part of the string if necessary.
+    // for strange reasons, the file path gets messed up on iOS (an extra "file:///" gets added in front of the path)
+    // . Thus, to fix this, I must remove that part of the string on iOS only.
     var filePath = recordingFile.path;
-    if (filePath.contains("file:///file:///")) filePath = filePath.substring(8, filePath.length);
+    if (Platform.isIOS) filePath = filePath.substring(8, filePath.length);
     final recordingFileAdjusted = File(filePath);
 
     // create a random identifier for the audio recording
